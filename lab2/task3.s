1c55c1e27f0541efe19752f7a3bef43487d62b81
Muhammad Mustafa M. Jawed <160347147+mustafajawed@users.noreply.github.com>
2026-09-07 15:45:58 +0500

    Refactor loops for array initialization and summation
    Refactor assembly code to initialize and sum an array.

--- lab2/task3.s
+++ lab2/task3.s
@@ -1,26 +1,32 @@
-li x22, 0 #i=0
-li x24 ,0x200 #array base address
-li x25, 10 #upper bound
+.text
+.globl main
+
+main:
+    li x24, 0x200       # base address of array
+    li x25, 10          # upper bound (array size = 10)
+
+
+    li x22, 0           # i = 0
 
 loop1:
-    bge x22, x25, end_loop
-    slli x26, x22, 2 #i*4
-    add x27, x24, x26 #array[i] address
-    addi x22, x22, 1
-    beq x0, x0, loop1
+    bge  x22, x25, end_loop1
+    slli x26, x22, 2    # offset = i * 4
+    add  x27, x24, x26   # &array[i]
+    sw   x22, 0(x27)    # array[i] = i 
+    addi x22, x22, 1    # i++
+    beq  x0,  x0,  loop1
 
-li x22, 0 #i=0
+end_loop1:
+    li x22, 0           # i = 0
+    li x23, 0           # sum = 0
+
 loop2:
-    bge x22, x25, end_loop2
-    slli x26, x22, 2 #i*4
-    add x27, x24, x26 #array[i] address
-    lw x28, 0(x27) #load array[i]
-    add x23, x23, x28 #sum += array[i]
-    addi x22, x22, 1
-    beq x0, x0, loop2
-
-
+    bge  x22, x25, end_loop2
+    slli x26, x22, 2    # offset = i * 4
+    add  x27, x24, x26   # &array[i]
+    lw   x28, 0(x27)    # load array[i]
+    add  x23, x23, x28   # sum += array[i]
+    addi x22, x22, 1    # i++
+    beq  x0,  x0,  loop2
 
 end_loop2:
-    
-end_loop:   
